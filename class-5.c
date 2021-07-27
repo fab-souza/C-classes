7b17dfe1ca3fd457a6958a6af56ac13d821b37f4
fab-souza <67301805+fab-souza@users.noreply.github.com>
2021-07-27 11:16:29 -0300

    Adding the fifth exercise to the list.
    This exercise asks for a number and returns the fifth part of it.

---
+++ class-5.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+main()
+
+{
+	float numero, quinto;
+	printf("Entre com um valor: ");
+	scanf("%f", &numero);
+	quinto = numero * 1/5;
+	printf("A quinta parte do número digitado é: %f", quinto);
+}
