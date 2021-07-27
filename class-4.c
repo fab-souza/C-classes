62d65d109331c2f7db15e37632d7a09344da8738
fab-souza <67301805+fab-souza@users.noreply.github.com>
2021-07-27 11:08:28 -0300

    Adding the fourth exercise
    This exercise asks for a number and gives the squared value of it.

---
+++ class-4.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+main()
+
+{
+	float numero, quadrado;
+	printf("Entre com um valor: ");
+	scanf("%f", &numero);
+	quadrado = numero * numero;
+	printf("O quadrado do número digitado é: %f", quadrado);
+}
