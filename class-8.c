70e0b7887d36ea4d6782ec785ef15a9407cea36f
fab-souza <67301805+fab-souza@users.noreply.github.com>
2021-08-22 14:37:06 -0300

    squared
    This code prompts you to input three numbers, then the code squares each one and displays their sum at the end.

---
+++ class-8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+
+{
+	float numx, numy, numz, x, y, z, fim;
+	printf("Digite o primeiro valor: \n");
+	scanf("%f", &numx);
+	printf("Digite o segundo valor: \n");
+	scanf("%f", &numy);
+	printf("Digite o terceiro valor: \n");
+	scanf("%f", &numz);
+	x = numx * numx;
+	y = numy * numy;
+	z = numz * numz;
+	fim = x + y + z;
+	printf("A soma dos quadrdos dos trEs  valores é: %.2f", fim);
+}
