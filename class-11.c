d8abbfa6dbb5ebe3163c026097cb877475429ef9
fab-souza <67301805+fab-souza@users.noreply.github.com>
2021-08-22 15:37:08 -0300

    'if' command
    This code contains the first comparison with the 'if' command, when comparing if the sum of two numbers is greater than 10.

---
+++ class-11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	int A, B, soma;
+	printf("Digite um número: \n");
+	scanf("%d", &A);
+	printf("Digite outro número: \n");
+	scanf("%d" &B);
+	soma = A + B;
+	printf("O valor da soma = %d \n", soma);
+	if (soma > 10);
+	{
+		printf("\nO valor da soma é maior do que 10.");
+	}
+	return(0);
+}
