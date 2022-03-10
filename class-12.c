c3efa397b9959f58cca59c60d2eb56ced4fac198
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-10 14:41:52 -0300

    Pede um nº e devolve a soma do triplo com o dobro

--- class-12.c
+++ class-12.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-main()
+main() 
 {
-  int
+  int numx, numy, numz, fim;
+  printf("Digite um valor: \n");
+  scanf("%d", &numx);
+  numy = 3*numx + 1;
+  numz = 2*numx - 1;
+  fim = numy + numz;
+  printf("O triplo do valor mais 1 é de: %d", numy);
+  printf("\nO dobro do valor menos 1 é de: %d", numz);
+  printf("\nA soma dos dois últimos valores é de: %d", fim);
+  return 0;
+}
