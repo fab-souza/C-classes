0d4a7efccc080ed51fb7bd4548ad133d76a239de
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-15 08:34:05 -0300

    Create class-23.c

---
+++ class-23.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void) 
+{
+  int numero;
+  printf("\nEste programa mostra na tela os números de 10 até 1:\n\n");
+  
+  for(numero=10; numero>=1; numero--)
+    {
+      printf("%d\n", numero);
+    }
+  
+}
