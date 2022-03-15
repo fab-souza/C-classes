ddf6f672c9558a1b03cc0a110b2be655d8802eab
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-15 08:33:13 -0300

    Repetição contada - somatório

---
+++ class-22.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void) 
+{
+  int numero;
+  printf("\nEste programa mostra na tela os números de 1 até 10:\n\n");
+  
+  for(numero=1; numero<=10; numero++)
+    {
+      printf("%d\n", numero);
+    }
+}
