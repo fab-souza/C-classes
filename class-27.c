8cd1abcfca6e567d3426f68df0a88afdc3645979
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-19 11:34:38 -0300

    Comando Do... While

---
+++ class-27.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() 
+{
+  int contador=0;
+  do
+    {
+      printf("%d\n", contador);
+      contador++;
+    } 
+    while (contador <=10);
+    {
+      printf("Acabou!\n");
+    }
+}
