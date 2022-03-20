d06ebe37b838893c343a94b155baf9a19068e2ef
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-20 19:57:29 -0300

    Calcula a área de 1 triângulo

--- class-14.c
+++ class-14.c
@@ -2,10 +2,12 @@
 
 main() 
 {
-  float numx, fim;
+  float base, altura, fim;
   printf("Digite o valor da base do triângulo (em metros): \n");
-  scanf("%f", &numx);
-  fim = (numx * numx)/2;
+  scanf("%f", &base);
+  printf("Digite o valor da altura do triângulo (em metros): \n");
+  scanf("%f", &altura);
+  fim = (base * altura)/2;
   printf("A área do triângulo é de: %.2f m².", fim);
   
   return 0;
